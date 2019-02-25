a0aea72a23eeeae436f8c312fa5d20f40c210137
Pascal Schoenfeld <kamus.foryou@gmx.net>
2019-02-25 18:24:43 +0100

    initial commit #2

--- .config/google-chrome/Default/Extensions/ffijoclmniipjbhecddgkfpdafpbdnen/2.10.0_0/popup/popupActivator.js
+++
@@ -1,6 +0,0 @@
-if (typeof document != undefined) {
-    document.body.style.visibility = 'visible';
-    let controller = location.search == '?integration' ?
-        new PagePopupController() :
-        new PopupController();
-}
