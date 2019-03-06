ddc3387e9939e9abecf21d5d59c02c5a899fc3a9
PascalSchoe <kamus.foryou@gmx.net>
2019-03-06 13:11:36 +0100

    fix which caused scripts folder to be unnecessary nested

--- .scripts/.scripts/confirm-prompt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# This script prompts the user with the message which is $1
-# After chosing either 'yes' or 'no' $2 will be executed
-options="Nein\nJa"
-chosen=$(echo "$options" | dmenu -i -p "$1")
-
-if [ "$chosen" = "Ja" ]; then
-  $2
-fi
