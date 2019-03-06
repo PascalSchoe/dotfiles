00bd1946a48b0e3761f151cacde457686ad3f9a7
PascalSchoe <kamus.foryou@gmx.net>
2019-03-06 13:04:11 +0100

    Folderstructure changed. exporting  via config-helper script

--- .scripts/.scripts/confirm-prompt.sh
+++ .scripts/.scripts/confirm-prompt.sh
@@ -1,4 +1,9 @@
+#!/bin/sh
 # This script prompts the user with the message which is $1
 # After chosing either 'yes' or 'no' $2 will be executed
-[ $(echo "Nein\nJa" | dmenu -i -p "$1") \
-  == "Ja" ] && $2
+options="Nein\nJa"
+chosen=$(echo "$options" | dmenu -i -p "$1")
+
+if [ "$chosen" = "Ja" ]; then
+  $2
+fi
